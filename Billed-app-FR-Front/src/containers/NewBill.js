f789190e91972a9c3d3d7680aa542f5c8bbb0f02
philippepecoraro <philippe.pecoraro@bbox.fr>
2022-06-01 16:58:12 +0200

    fix bug hunt - bills

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -1,5 +1,6 @@
 import { ROUTES_PATH } from '../constants/routes.js'
 import Logout from "./Logout.js"
+let extension = "false";
 
 export default class NewBill {
     constructor({ document, onNavigate, store, localStorage }) {
@@ -13,52 +14,59 @@ export default class NewBill {
         this.fileUrl = null
         this.fileName = null
         this.billId = null
+        this.formData = new FormData;
         new Logout({ document, localStorage, onNavigate })
     }
     handleChangeFile = e => {
         e.preventDefault()
         const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-        const filePath = e.target.value.split(/\\/g)
-        const fileName = filePath[filePath.length - 1]
-        const formData = new FormData()
-        const email = JSON.parse(localStorage.getItem("user")).email
-        formData.append('file', file)
-        formData.append('email', email)
-
-        this.store
-            .bills()
-            .create({
-                data: formData,
-                headers: {
-                    noContentType: true
-                }
-            })
-            .then(({ fileUrl, key }) => {
-                console.log(fileUrl)
-                this.billId = key
-                this.fileUrl = fileUrl
-                this.fileName = fileName
-            }).catch(error => console.error(error))
+        if (file.type === "image/jpg" || file.type === "image/jpeg" || file.type === "image/png") {
+            extension = "true";
+            const filePath = e.target.value.split(/\\/g)
+            this.fileName = filePath[filePath.length - 1]
+            const email = JSON.parse(localStorage.getItem("user")).email
+            this.formData.append('file', file)
+            this.formData.append('email', email)
+        } else {
+            alert("Veuillez choisir un fichier Image avec une extension .jpg ou .jpeg ou .png");
+        }
     }
     handleSubmit = e => {
         e.preventDefault()
-        console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-        const email = JSON.parse(localStorage.getItem("user")).email
-        const bill = {
-            email,
-            type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-            name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-            amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-            date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
-            vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-            pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-            commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-            fileUrl: this.fileUrl,
-            fileName: this.fileName,
-            status: 'pending'
+        if (extension === "true") {
+            console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+            const email = JSON.parse(localStorage.getItem("user")).email
+            this.store
+                .bills()
+                .create({
+                    data: this.formData,
+                    headers: {
+                        noContentType: true
+                    }
+                })
+                .then(({ fileUrl, key }) => {
+                    console.log(fileUrl)
+                    this.billId = key
+                    this.fileUrl = fileUrl
+                    const bill = {
+                        email,
+                        type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+                        name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+                        amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+                        date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+                        vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+                        pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+                        commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+                        fileUrl: this.fileUrl,
+                        fileName: this.fileName,
+                        status: 'pending'
+                    }
+                    this.updateBill(bill)
+                    this.onNavigate(ROUTES_PATH['Bills'])
+                }).catch(error => console.error(error))
+        } else {
+            alert("Veuillez choisir un fichier Image avec une extension .jpg ou .jpeg ou .png");
         }
-        this.updateBill(bill)
-        this.onNavigate(ROUTES_PATH['Bills'])
     }
 
     // not need to cover this function by tests
